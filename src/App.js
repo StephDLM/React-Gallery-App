a7bd132276fd388fa7cf5090fdc1e6f450d7db94
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-18 15:37:53 -0700

    uploaded react app and moved files around

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 
+//for later[]
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+
 //import component in config
 import apiKey from './Config';
+
+//adding search bar
+performSearch = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });    
+  }
+
 
 
 componentDidMount() ;{
---
+++ src/Components/PhotosList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Photos from './Photos';
+
+const GifList = props => { 
+  
+  return(
+    <ul className="photos-list">
+      {/* <Gif /> */}
+    </ul> 
+  );
+}
+
+export default PhotosList;
