821447025a9d3cafc6de59ac9430bd70e7d35bca
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-22 16:32:34 -0700

    fixed home page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component {
     super(); //lets call "this" inside the constructor
     this.state = { //collection of objects
       photos: [],
-      greatdanes: [],
+      greatdane: [],
       lakes: [],
       trees: []
     };
@@ -25,21 +25,23 @@ export default class App extends Component {
 //video: giffy react course to pass props and iterate over that data map. --> ph 
   componentDidMount() { 
     this.performSearch(); //lifecycle method triggered-- default query
-    this.performSearch('greatdanes'); //lifecycle method triggered
+    this.performSearch('greatdane'); //lifecycle method triggered
     this.performSearch('lakes'); //lifecycle method triggered
     this.performSearch('trees'); //lifecycle method triggered
   
   }
   
-  performSearch = (query = 'greatdanes') => { //fetching the data and putting in the array --> call this whenever performing a search 
+  performSearch = (query = 'chairs') => { //fetching the data and putting in the array --> call this whenever performing a search 
     fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => response.json())
     .then(data => { 
       console.log(data)
       console.log(this.performSearch);
         if (query === 'greatdane'){
-          this.setState({ greatdane: data.photos.photo});         
-        } else if (query === 'lakes'){
+          this.setState({ greatdane: data.photos.photo}); 
+          console.log(this.state.greatdane);          
+        }
+          else if (query === 'lakes'){
           this.setState({ lakes: data.photos.photo});           
         } else if (query === 'trees'){
           this.setState({ trees: data.photos.photo});         
@@ -58,14 +60,22 @@ export default class App extends Component {
       render() { 
         console.log(this.state.photos);
         return (
+          
           <BrowserRouter>
             <div className="container">
                 <h1 className="main-title"> Steph's Photo Search </h1>
                 <SearchForm onSearch={this.performSearch} />
-              </div>  
-            <div className="main-content">
+            </div> 
+            <Nav/>
+            <Switch>           
+              <Route  exact path='/' render={() => <PhotoList data={this.state.photos} />} />
+              <Route  exact path='/greatdane' render={() => <PhotoList data={this.state.greatdane} />} /> 
+              <Route  exact path='/lakes' render={() => <PhotoList data={this.state.lakes} />} /> 
+              {/* <Route  exact path='/lakes' render={() => <PhotoList data={this.state.sculpture} />} />  */} */
+            </Switch>
+            {/* <div className="main-content">
               <PhotoList data={this.state.photos} />
-            </div>
+            </div> */}
           </BrowserRouter>
         );
       }
@@ -73,14 +83,13 @@ export default class App extends Component {
     
 }
 //image handler--> need to add, 
-
-<div>
-<Route exact path='/'
-render={ () => <PhotoList to={this.state.photo} /> } />
-<Route exact path='/greatdanes'
-render={ () => <PhotoList data={this.state.greatdanes} /> } />         
-<Route exact path='/lakes'
-render={ () => <PhotoList data={this.state.lakes} /> } />      
-<Route exact path='/trees'
-render={ () => <PhotoList data={this.state.trees} /> } />   
-</div>
+// <div>
+// <Route exact path='/'
+// render={ () => <PhotoList to={this.state.photo} /> } />
+// <Route exact path='/greatdanes'
+// render={ () => <PhotoList data={this.state.greatdanes} /> } />         
+// <Route exact path='/lakes'
+// render={ () => <PhotoList data={this.state.lakes} /> } />      
+// <Route exact path='/trees'
+// render={ () => <PhotoList data={this.state.trees} /> } />   
+// </div>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import { NavLink } from 'react-router-dom';
 import PhotoList from './PhotoList';
 import { withRouter, NavLink } from 'react-router-dom'; 
 
 
+const Nav = (props) => {
 
-class Nav extends Component { 
-    constructor(){
-        super()
-        this.state = {
-            button: ''
-        }}
+// class Nav extends Component { 
+    // constructor(){
+    //     super()
+        // th = {
+        //     button: ''
+        // }
         // }};
 //create a handleclick
 //onclick handler is getting text content and put it to lower case and set state the state of the property to teh value 
-handleClick = (e) => {
-    this.props.onClick(e.target.textContent.toLowerCase());
-    this.setState({button: e.target.textContent.toLowerCase()}) 
-};
-    render(){
-        return (
-            <head>
+// handleClick = (e) => {
+//     this.prop.onClick(e.target.textContent.toLowerCase());
+//     this.setState({button: e.target.textContent.toLowerCase()}) 
+// };
+  
+    return (
             <nav className="main-nav">
             <ul>
-              <li><NavLink to="/greatdanes" onClick = {this.handleClick}>Great Danes</NavLink></li>
-              <li><NavLink to="/lakes" onClick = {this.handleClick}>Lakes</NavLink></li>
-              <li><NavLink to="/trees" onClick = {this.handleClick}>Trees</NavLink></li>
+              <li><NavLink to="/greatdane" >Great Danes</NavLink></li>
+              <li><NavLink to="/lakes" >Lakes</NavLink></li>
+              <li><NavLink to="/trees" >Trees</NavLink></li>
             </ul>    
           </nav>
-          </head>
-)}
-        }        
+)};
+               
     // };
-export default withRouter(Nav);
+export default Nav;
+
+//onClick = {this.handleClick}
